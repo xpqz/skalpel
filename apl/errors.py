c4e0c47b389608031c9088ab9ffc284130616b1f
Stefan Kruger <stefan.kruger@gmail.com>
2022-08-11 16:10:01 +0100

    Errors in a separate file

---
+++ apl/errors.py
@@ -0,0 +1,23 @@
+class ArityError(Exception):
+    pass
+
+class EmitError(Exception):
+    pass
+
+class LengthError(Exception):
+    pass
+
+class LimitError(Exception):
+    pass
+
+class NYIError(Exception):
+    pass
+
+class RankError(Exception):
+    pass
+
+class UnexpectedToken(Exception):
+    pass
+
+class ValueError(Exception):
+    pass
