bec340bf61d53db84918fd078853f152630c8833
Stefan Kruger <stefan.kruger@gmail.com>
2022-09-09 11:23:31 +0100

    Tests for tokenising dfns

--- tests/test_tokeniser.py
+++ tests/test_tokeniser.py
@@ -81,8 +81,48 @@ class TestTokeniser:
         assert tokens[16].kind == TokenType.NAME
         assert tokens[16].tok == 'var'
 
-    def test_code2(self):
-        src = "×⍨ 4.5 - (4 ¯3 5.6)"
+    def test_dfn(self):
+        src = "dfn ← {⍺>0: 5 ⋄ ⍺+⍵}"
         tokeniser = Tokeniser(src)
         tokens = tokeniser.lex()
+        assert tokens[0].kind == TokenType.EOF
+
+        assert tokens[1].kind == TokenType.NAME
+        assert tokens[1].tok == 'dfn'
+
+        assert tokens[2].kind == TokenType.GETS
+        assert tokens[2].tok == '←'
+
+        assert tokens[3].kind == TokenType.LBRACE
+        assert tokens[3].tok == '{'
+
+        assert tokens[4].kind == TokenType.ALPHA
+        assert tokens[4].tok == '⍺'
+
+        assert tokens[5].kind == TokenType.FUN
+        assert tokens[5].tok == '>'
+
+        assert tokens[6].kind == TokenType.SCALAR
+        assert tokens[6].tok == 0
+
+        assert tokens[7].kind == TokenType.COLON
+        assert tokens[7].tok == ':'
+
+        assert tokens[8].kind == TokenType.SCALAR
+        assert tokens[8].tok == 5
+
+        assert tokens[9].kind == TokenType.DIAMOND
+        assert tokens[9].tok == '⋄'
+
+        assert tokens[10].kind == TokenType.ALPHA
+        assert tokens[10].tok == '⍺'
+
+        assert tokens[11].kind == TokenType.FUN
+        assert tokens[11].tok == '+'
+
+        assert tokens[12].kind == TokenType.OMEGA
+        assert tokens[12].tok == '⍵'
+        
+        assert tokens[13].kind == TokenType.RBRACE
+        assert tokens[13].tok == '}'
 
